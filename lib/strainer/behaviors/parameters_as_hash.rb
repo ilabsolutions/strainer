17328969f636170b594084cf0c42c00ae441cd5a
Rishab Govind <rishab.govind@gmail.com>
2020-02-24 12:49:27 -0500

    UNTESTED: Added support for edit-in-place methods. For use maybe sometime later.

--- lib/strainer/behaviors/parameters_as_hash.rb
+++ lib/strainer/behaviors/parameters_as_hash.rb
@@ -6,25 +6,45 @@ module Strainer
   module Behaviors
     # Allows Hash methods to be called on an ActionController::Parameters object which no longer
     # works in rails 6 since it doesn't descend from Hash.
-    # params.merge(:params)
+    # params.merge(default_params)
+    # For unsafe methods such as merge! we modify the internal params hash
     class ParametersAsHash < Strainer::RuntimeBehavior
       module BehavesHashlike
         include Strainer::Logable
         HASH_INSTANCE = {}.freeze
+        UNSAFE_METHODS = %i[select! filter! reject! compact! transform_keys! transform_values! merge!].freeze
+        private_constant :HASH_INSTANCE, :UNSAFE_METHODS
 
         def method_missing(method_name, *args, &block)
-          if HASH_INSTANCE.respond_to?(method_name)
-            # create a hash copy so that unsafe methods can't be called on the internal hash.
-            hash = to_h
-            strainer_log('PARAMS_AS_HASH', custom: { hash_method: method_name })
-            hash.send(method_name, *args, &block)
-          else
-            super
-          end
+          return super unless forwardable?(method_name)
+
+          strainer_log('PARAMS_AS_HASH', custom: { hash_method: method_name, unsafe: unsafe?(method_name) })
+
+          # This method always returns a hash and not ActionController::Parameters.
+          # This is on purpose since we're assuming that if you are using an action that treats the params as hash
+          # AND are using the return value, then you actually are currently handling of the return as a hash.
+          # Since we use strainer only until we can fix the rails compatibility issues, this should be ok to do.
+          target_hash.send(method_name, *args, &block)
         end
 
         def respond_to_missing?(method_name, include_private = false)
-          HASH_INSTANCE.respond_to?(method_name) || super
+          forwardable?(method_name) || super
+        end
+
+        private
+
+        def target_hash(method_name)
+          # only allow a whitelisted list of unsafe methods to operate on the internal parameters hash
+          unsafe?(method_name) ? @parameters : to_h
+        end
+
+        def unsafe?(method_name)
+          UNSAFE_METHODS.include?(method_name)
+        end
+
+        def forwardable?(method_name)
+          # forwardable only if hash responds to a public method
+          HASH_INSTANCE.respond_to?(method_name)
         end
       end
 
