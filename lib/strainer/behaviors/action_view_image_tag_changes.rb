a6d963eebd6b40200285192e2b84d020591c7b7b
Nikhil Hira <nikhil.hira@agilent.com>
2020-10-07 08:25:28 -0700

    add logging

--- lib/strainer/behaviors/action_view_image_tag_changes.rb
+++ lib/strainer/behaviors/action_view_image_tag_changes.rb
@@ -7,8 +7,13 @@ module Strainer
 
     class ActionViewImageTagChanges < Strainer::RuntimeBehavior
       module HandleNilImageTagSrc
+        include Strainer::Logable
+
         def image_tag(source, options={})
-          return tag("img", options) if source.nil?
+          if source.nil?
+            strainer_log('IMAGE_TAG_SOURCE_NIL', custom: { source: source, options: options })
+            return tag("img", options)
+          end
 
           super
         end
