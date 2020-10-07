21a7c52cb5e4c3d1a7a958ed8fe1af26662e9790
Nikhil Hira <nikhil.hira@agilent.com>
2020-10-07 05:04:50 -0700

    https://ilabdev.atlassian.net/browse/ILAB-15026
    changes to Rails 6 result in broken landing page if header_logo is nil. This change replaces nil with a string so it will load.

---
+++ lib/strainer/behaviors/action_view_image_tag_helper.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+require 'action_view/helpers/asset_tag_helper'
+
+module Strainer
+  module Behaviors
+
+    class ActionViewImageTagHelperChanges < Strainer::RuntimeBehavior
+      module HandleNilImageTagSrc
+        def image_tag(source, options={})
+          return tag("img", options) if source.nil?
+
+          super
+        end
+      end
+
+      def apply_patch!
+        ActionView::Helpers::AssetTagHelper.prepend(HandleNilImageTagSrc)
+      end
+    end
+  end
+end
